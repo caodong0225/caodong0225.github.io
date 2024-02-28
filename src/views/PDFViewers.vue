32444df14719bd1ba332d5ec79b18570b17c83af
曹东 <867137498@qq.com>
2024-02-28 19:35:27 +0800

    添加了pdfviewer组件

--- src/views/PDFViewers.vue
+++ src/views/PDFViewers.vue
@@ -1,16 +1,25 @@
 <template>
-  <div>
-    <!-- 使用动态生成的 PDF 文件 URL 在 iframe 中显示 PDF 文件 -->
-    <iframe :src="pathUrl" scrolling="auto" frameborder="0" style="width: 100%; height: 900px;"></iframe>
+  <!-- 使用动态生成的 PDF 文件 URL 在 iframe 中显示 PDF 文件 -->
+  <!--
+  <iframe :src="pathUrl" scrolling="auto" frameborder="0" style="width: 100%; height: 900px;"></iframe>
+  -->
+  <div style="padding-top: 56px">
+    <vue-pdf-app style="height: 100vh;" :pdf="pathUrl"></vue-pdf-app>
   </div>
 </template>
 
 <script>
+import VuePdfApp from "vue3-pdf-app";
+// import this to use default icons for buttons
+import "vue3-pdf-app/dist/icons/main.css";
 export default {
   data () {
     return {
       pathUrl:''
     }
+  },
+  components: {
+    VuePdfApp
   },
   mounted () {
     // 根据路由参数获取 PDF 文件名
