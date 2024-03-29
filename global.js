6707e1e8272e7b9126532374acb214a8ea4c69a3
曹东 <867137498@qq.com>
2024-03-29 14:16:25 +0800

    更新了博客文章的命名规则，添加了新文章

--- global.js
+++ global.js
@@ -9,28 +9,35 @@ const GLOBAL = {
     ],
     articles: [
         {month:'2023年01月', content:[
-            {picture: 'static/images/pic1.jpg', url: '/archives/article1', date: '2023年01月06日', title: '人工智能学习笔记一——激活函数', tags: ['人工智能']},
-            {picture: 'static/images/pic2.png', url: '/archives/article2', date: '2023年01月08日', title: '人工智能学习笔记二——损失函数', tags: ['人工智能']},
-            {picture: 'static/images/pic3.jpg', url: '/archives/article3', date: '2023年01月10日', title: '人工智能学习笔记三——卷积神经网络', tags: ['人工智能', '计算机视觉']},
-            {picture: 'static/images/pic4.jpg', url: '/archives/article4', date: '2023年01月14日', title: '人工智能学习笔记四——手写数字识别', tags: ['人工智能', '计算机视觉']},
-            {picture: 'static/images/pic5.png', url: '/archives/article5', date: '2023年01月20日', title: 'CTF学习笔记一——RSA加密', tags: ['CTF', '密码学']},
-            {picture: 'static/images/pic6.png', url: '/archives/article6', date: '2023年01月21日', title: '人工智能学习笔记五——孪生神经网络', tags: ['人工智能']},
-            {picture: 'static/images/pic7.jpg', url: '/archives/article7', date: '2023年01月24日', title: 'CTF学习笔记二——ECC加密', tags: ['CTF', '密码学']},
+            {picture: 'static/images/激活函数.jpg', url: '/archives/激活函数', date: '2023年01月06日', title: '人工智能学习笔记一——激活函数', tags: ['人工智能']},
+            {picture: 'static/images/神经网络.png', url: '/archives/神经网络', date: '2023年01月08日', title: '人工智能学习笔记二——损失函数', tags: ['人工智能']},
+            {picture: 'static/images/卷积神经网络.jpg', url: '/archives/卷积神经网络', date: '2023年01月10日', title: '人工智能学习笔记三——卷积神经网络', tags: ['人工智能', '计算机视觉']},
+            {picture: 'static/images/手写数字识别.jpg', url: '/archives/手写数字识别', date: '2023年01月14日', title: '人工智能学习笔记四——手写数字识别', tags: ['人工智能', '计算机视觉']},
+            {picture: 'static/images/RSA加密.png', url: '/archives/RSA加密', date: '2023年01月20日', title: 'CTF学习笔记一——RSA加密', tags: ['CTF', '密码学']},
+            {picture: 'static/images/孪生神经网络.png', url: '/archives/孪生神经网络', date: '2023年01月21日', title: '人工智能学习笔记五——孪生神经网络', tags: ['人工智能']},
+            {picture: 'static/images/ECC加密.jpg', url: '/archives/ECC加密', date: '2023年01月24日', title: 'CTF学习笔记二——ECC加密', tags: ['CTF', '密码学']},
             ]
         },
         {month:'2023年03月', content:[
-            {picture: 'static/images/pic8.jpg', url: '/archives/article8', date: '2023年03月14日', title: '人工智能学习笔记六——CBOW模型', tags: ['人工智能', '自然语言处理']},
-            {picture: 'static/images/pic9.png', url: '/archives/article9', date: '2023年03月14日', title: '人工智能学习笔记七——基于BiLstm的恶意url检测', tags: ['人工智能', '自然语言处理']},
+            {picture: 'static/images/CBOW模型.jpg', url: '/archives/CBOW模型', date: '2023年03月14日', title: '人工智能学习笔记六——CBOW模型', tags: ['人工智能', '自然语言处理']},
+            {picture: 'static/images/基于BiLstm的恶意url检测.png', url: '/archives/基于BiLstm的恶意url检测', date: '2023年03月14日', title: '人工智能学习笔记七——基于BiLstm的恶意url检测', tags: ['人工智能', '自然语言处理']},
+            {picture: 'static/images/Pohlig-Hellman算法.jpg', url: '/archives/Pohlig-Hellman算法', date: '2023年03月29日', title: 'CTF学习笔记三——Pohlig-Hellman算法', tags: ['CTF', '密码学']},
+            {picture: 'static/images/LCG发生器.png', url: '/archives/LCG发生器', date: '2023年03月29日', title: 'CTF学习笔记四——LCG发生器', tags: ['CTF', '密码学']},
         ]
         },
         {month:'2023年04月', content:[
-            {picture: 'static/images/pic10.jpg', url: '/archives/article10', date: '2023年04月09日', title: '人工智能学习笔记八——基于深度学习的钓鱼短信检测', tags: ['人工智能', '自然语言处理']},
-            {picture: 'static/images/pic11.jpg', url: '/archives/article11', date: '2023年04月09日', title: '人工智能学习笔记九——基于transformer的恶意网页识别', tags: ['人工智能', '自然语言处理']},
+            {picture: 'static/images/基于深度学习的钓鱼(垃圾)短信检测.jpg', url: '/archives/基于深度学习的钓鱼(垃圾)短信检测', date: '2023年04月09日', title: '人工智能学习笔记八——基于深度学习的钓鱼短信检测', tags: ['人工智能', '自然语言处理']},
+            {picture: 'static/images/基于transformer的恶意网页识别.jpg', url: '/archives/基于transformer的恶意网页识别', date: '2023年04月09日', title: '人工智能学习笔记九——基于transformer的恶意网页识别', tags: ['人工智能', '自然语言处理']},
         ]
         },
+        {month:'2023年06月', content:[
+                {picture: 'static/images/pwn刷题记录.jpg', url: '/archives/pwn刷题记录', date: '2023年06月22日', title: 'pwn刷题记录', tags: ['CTF']},
+            ]
+        },
         {month:'2023年08月', content: [
-            {picture: 'static/images/pic12.png', url: '/archives/article12', date: '2023年08月29日', title: '人工智能学习笔记十——基于aaegan的手写英文欺骗样本生成', tags: ['人工智能']},
-            {picture: 'static/images/pic13.png', url: '/archives/article13', date: '2023年08月29日', title: '人工智能学习笔记十一——基于crnn+ctc的验证码识别机制', tags: ['人工智能', '计算机视觉']},
+            {picture: 'static/images/springboot+mybatisplus框架.png', url: '/archives/springboot+mybatisplus框架', date: '2023年08月23日', title: 'Java学习笔记一——springboot+mybatisplus框架', tags: ['Java']},
+            {picture: 'static/images/基于aaegan的手写英文欺骗样本生成.png', url: '/archives/基于aaegan的手写英文欺骗样本生成', date: '2023年08月29日', title: '人工智能学习笔记十——基于aaegan的手写英文欺骗样本生成', tags: ['人工智能']},
+            {picture: 'static/images/基于crnn+ctc的验证码识别机制.png', url: '/archives/基于crnn+ctc的验证码识别机制', date: '2023年08月29日', title: '人工智能学习笔记十一——基于crnn+ctc的验证码识别机制', tags: ['人工智能', '计算机视觉']},
         ]
         }
     ]
