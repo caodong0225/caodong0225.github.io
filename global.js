f63fa84076e5019a26fbd135ad14e47f7f6e1f59
曹东 <867137498@qq.com>
2025-05-20 22:29:34 +0800

    feat: 添加新的CTF文章

--- global.js
+++ global.js
@@ -3,7 +3,7 @@ const GLOBAL = {
     author_name : "caodong0225",
     games: [
         {picture: 'static/images/1.png', url: '/game/index2', date: '2022年07月10日', title: '五子棋小游戏', description: '五子棋小游戏，目前单人模式还没做完，悔棋功能无法使用', tags: ['unity游戏开发']},
-        {picture: 'static/images/2.png', url: '/game/index1', date: '2023年01月05日', title: '井字棋小游戏', description: '井字棋小游戏，加入了极大极小值算法，可以单人和AI对战', tags: ['unity游戏开发','极大极小值算法']},
+        {picture: 'static/images/2.png', url: '/game/index1', date: '2023年01月05日', title: '井字棋小游戏', description: '井字棋小游戏，加入了极大极小值算法，可以单人和AI对战', tags: ['unity游戏开发']},
         {picture: 'static/images/3.png', url: '/game/index3', date: '2023年08月04日', title: '贪吃蛇小游戏', description: '贪吃蛇小游戏，Unity练手用的', tags: ['unity游戏开发']},
         {picture: 'static/images/4.png', url: '/game/index4', date: '2023年09月16日', title: '纽约市地图', description: '第一次做3D游戏，把下载下来的两个素材整合了一下', tags: ['unity游戏开发']},
     ],
@@ -18,6 +18,10 @@ const GLOBAL = {
             {picture: 'static/images/ECC加密.jpg', url: '/archives/ECC加密', date: '2023年01月24日', title: 'CTF学习笔记二——ECC加密', tags: ['CTF', '密码学']},
             ]
         },
+        {month:'2023年02月', content:[
+                {picture: 'static/images/t01e7d22573828a8a81.jpg', url: '/archives/Real world ctf密码学题目讲解', date: '2023年02月1日', title: '23年Real World CTF个人wp', tags: ['CTF']},
+            ]
+        },
         {month:'2023年03月', content:[
             {picture: 'static/images/CBOW模型.jpg', url: '/archives/CBOW模型', date: '2023年03月14日', title: '人工智能学习笔记六——CBOW模型', tags: ['人工智能', '自然语言处理']},
             {picture: 'static/images/基于BiLstm的恶意url检测.png', url: '/archives/基于BiLstm的恶意url检测', date: '2023年03月14日', title: '人工智能学习笔记七——基于BiLstm的恶意url检测', tags: ['人工智能', '自然语言处理']},
@@ -28,10 +32,19 @@ const GLOBAL = {
         {month:'2023年04月', content:[
             {picture: 'static/images/基于深度学习的钓鱼(垃圾)短信检测.jpg', url: '/archives/基于深度学习的钓鱼(垃圾)短信检测', date: '2023年04月09日', title: '人工智能学习笔记八——基于深度学习的钓鱼短信检测', tags: ['人工智能', '自然语言处理']},
             {picture: 'static/images/基于transformer的恶意网页识别.jpg', url: '/archives/基于transformer的恶意网页识别', date: '2023年04月09日', title: '人工智能学习笔记九——基于transformer的恶意网页识别', tags: ['人工智能', '自然语言处理']},
+            {picture: 'static/images/红明谷.jpg', url: '/archives/红明谷密码学', date: '2023年04月20日', title: '23年红明谷杯密码学Wp', tags: ['CTF']},
         ]
+        },
+        {month:'2023年05月', content:[
+                {picture: 'static/images/ISCC.jpg', url: '/archives/ISCC个人wp', date: '2023年05月25日', title: '23年ISCC数据思维赛个人wp', tags: ['CTF']},
+            ]
         },
         {month:'2023年06月', content:[
                 {picture: 'static/images/pwn刷题记录.jpg', url: '/archives/pwn刷题记录', date: '2023年06月22日', title: 'pwn刷题记录', tags: ['CTF']},
+            ]
+        },
+        {month:'2023年07月', content:[
+                {picture: 'static/images/20200612003019272.png', url: '/archives/数学建模论文', date: '2023年07月30日', title: '上理工数学建模比赛校赛个人Wp', tags: ['数学建模']},
             ]
         },
         {month:'2023年08月', content: [
@@ -39,7 +52,20 @@ const GLOBAL = {
             {picture: 'static/images/基于aaegan的手写英文欺骗样本生成.png', url: '/archives/基于aaegan的手写英文欺骗样本生成', date: '2023年08月29日', title: '人工智能学习笔记十——基于aaegan的手写英文欺骗样本生成', tags: ['人工智能']},
             {picture: 'static/images/基于crnn+ctc的验证码识别机制.png', url: '/archives/基于crnn+ctc的验证码识别机制', date: '2023年08月29日', title: '人工智能学习笔记十一——基于crnn+ctc的验证码识别机制', tags: ['人工智能', '计算机视觉']},
         ]
-        }
+        },
+        {month:'2024年03月', content: [
+                {picture: 'static/images/pwn刷题记录.jpg', url: '/archives/第二届上理工信息安全竞赛个人writeup', date: '2024年03月30日', title: '第二届上理工信息安全竞赛个人writeup', tags: ['CTF']},
+            ]
+        },
+        {month:'2024年05月', content: [
+                {picture: 'static/images/OIP2.jpg', url: '/archives/writeup', date: '2024年05月19日', title: '2024华东南CISCN初赛wp', tags: ['CTF']},
+                {picture: 'static/images/R-C.jpg', url: '/archives/wp', date: '2024年05月25日', title: '2024上海市网络安全大赛wp', tags: ['CTF']},
+            ]
+        },
+        {month:'2025年04月', content: [
+                {picture: 'static/images/OIP.jpg', url: '/archives/writeup_gr', date: '2025年04月20日', title: '第三届蓝桥杯网安赛道个人Wp', tags: ['CTF']},
+            ]
+        },
     ]
 };
 
